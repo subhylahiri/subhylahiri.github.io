d9fae6a2b46f05f23664264408b9e4d1ed2ba665
Subhaneil Lahiri <subhylahiri@gmail.com>
2020-03-21 09:10:45 -0700

    json for nav

--- nav.js
+++ nav.js
@@ -1,35 +1,54 @@
-function buildNav(activeID, baseURL) {
-    if (baseURL === undefined) {
-        baseURL = '';
-    }
-    var body = document.getElementsByTagName("body");
-    var headDiv = document.getElementsByClassName("header");
-    var navDiv = document.createElement("div");
-    navDiv.className = "nav";
-    var navString =
-        '<ul>' +
-        '<li id="home"><a href="' + baseURL + 'index.html">Home</a></li>' +
-        '<li id="pubs"><a href="https://scholar.google.com/citations?user=2nEwLGcAAAAJ">Publications</a></li>' +
-        '<li id="subj"><a href="' + baseURL + 'research.html">Research</a></li>' +
-        '<li id="pres"><a href="' + baseURL + 'presentations/index.html">Presentations</a></li>' +
-        '</ul>';
-    navDiv.innerHTML = navString;
-    body[0].insertBefore(navDiv, headDiv[0])
-    headDiv[0].style.paddingTop = "0em";
-    var activeObj = document.getElementById(activeID);
-    activeObj.className = "active";
+function buildPage(activeID, baseURL) {
+    buildNav(activeID, baseURL);
+    buildFoot();
 }
 
 function buildFoot() {
-    var body = document.getElementsByTagName("body");
-    var footDiv = document.createElement("div");
+    let body = document.getElementsByTagName("body");
+    let footDiv = document.createElement("div");
     footDiv.className = "footer";
     footDiv.innerHTML = '<address>Subhaneil Lahiri: <tt>sulahiri at stanford dot edu</tt>. ' +
         '<a href="https://github.com/subhylahiri/subhylahiri.github.io">[Source]</a>.</address>';
     body[0].appendChild(footDiv);
 }
 
-function buildPage(activeID, baseURL) {
-    buildNav(activeID, baseURL);
-    buildFoot();
+function buildNav(activeID, baseURL) {
+    if (baseURL === undefined) {
+        baseURL = '';
+    }
+    getJSON(baseURL + "navData.json")
+    .then((navData) => {
+        let body = document.getElementsByTagName("body");
+        let headDiv = document.getElementsByClassName("header");
+        let navDiv = document.createElement("div");
+        navDiv.className = "nav";
+        let navString = '<ul>';
+        for (let i = 0; i < navData.length; i++) {
+            navString += entryNav(navData[i], baseURL);
+        }
+        navDiv.innerHTML = navString + '</ul>';
+        body[0].insertBefore(navDiv, headDiv[0])
+        headDiv[0].style.paddingTop = "0em";
+        let activeObj = document.getElementById(activeID);
+        activeObj.className = "active";
+    });
+}
+
+function getDir() {
+    let loc = window.location.pathname;
+    let dir = loc.substring(0, loc.lastIndexOf('/'));
+    return dir + '/'
+}
+
+async function getJSON(dataURL) {
+    const response = await fetch(getDir() + dataURL);
+    return await response.json();
+}
+
+function entryNav(entry, baseURL) {
+    let listItem = `<li id="${entry.id}"><a href="`;
+    if (entry.internal) {
+        listItem += baseURL;
+    }
+    return `${listItem + entry.url}">${entry.name}</a></li>`;
 }
