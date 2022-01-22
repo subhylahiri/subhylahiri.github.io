c4dd3ca190d2472a193342b7e2b4a1c0e37d060e
Subhaneil Lahiri <subhylahiri@gmail.com>
2022-01-22 00:26:26 +0000

    messages for ie

---
+++ scripts/ie.js
@@ -0,0 +1,9 @@
+
+function showIE() {
+    let paras = document.getElementsByClassName("ie");
+    for (let i = 0; i < paras.length; i++) {
+        paras[i].style.display = "block";
+    }
+}
+
+showIE();
